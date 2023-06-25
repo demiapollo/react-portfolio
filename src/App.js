1dd82f2ba36c9f7d118707e61eeb722445cd874d
demiapollo <apollodemirel67@gmail.com>
2023-06-24 21:50:39 -0700

    inital commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import { 
+  Typography 
+} from "@material-ui/core";
+
+
+function App() {
+  return (
+    <div className="App">
+      <Typography>Hello World</Typography>
+    </div>
+  );
+}
+
+export default App;
