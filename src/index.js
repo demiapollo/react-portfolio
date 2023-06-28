950c0ef01e8b05c86ddf2ece8d718b2dd04894d8
demiapollo <apollodemirel67@gmail.com>
2023-06-27 17:39:11 -0700

    navbar

--- src/index.js
+++ src/index.js
@@ -1,9 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-// import { BrowserRouter } from 'react-router-dom';
+
+import ButtonAppBar from './components/Navbar';
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
  
 root.render(  
+      <div>
+      <ButtonAppBar />
       <App />
+      </div>
 );
